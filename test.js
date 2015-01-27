531c536151a4b4b7aedc946fca2386693f765440
Dan Newcome <dan@dnuke.com>
2015-01-27 02:20:36 -0800

    refactor, tests

--- test.js
+++ test.js
@@ -1,9 +1,24 @@
 var assert = require('chai').assert;
+var doCalendar = require('./calendar');
+
+var jsdom = require("jsdom");
 
 describe('test suite', function() {
-	it('should work', function() {
-		console.log('works');	
+	it('should work', function(done) {
+		this.timeout = 5000;
+		var events = [{start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670}];
 		var expected = [{"start":30,"end":150,"col":0,"depth":0,"width":600,"left":0},{"start":540,"end":600,"col":0,"clear":true,"depth":1,"width":300,"left":0},{"start":560,"end":620,"col":1,"depth":1,"width":300,"left":300},{"start":610,"end":670,"col":0,"depth":0,"width":300,"left":0}]
-		assert.equal(1, 2);
+
+		jsdom.env({
+		  file: "index.html",
+		  scripts: ["calendar.js"],
+		  done: function (errors, window) {
+			setTimeout(function() {
+				var actual = window.doCalendar(events);
+				assert.deepEqual(actual, expected);
+				done();
+			}, 1000); 
+		  }}
+		);
 	}); 
 }); 
